657ab5e8f58aa0a72d33d5c20e642ace361667f9
zephyz <zephyz.zhaar@gmail.com>
2015-04-18 16:13:55 +0200

    added storyboard mockup

---
+++ My plant and me/PlantComunicator.swift
@@ -0,0 +1,29 @@
+//
+//  PlantComunicator.swift
+//  My plant and me
+//
+//  Created by zephyz on 18/04/15.
+//  Copyright (c) 2015 purplicious. All rights reserved.
+//
+
+import Foundation
+
+class PlantComunicator {
+//    let url = NSURL(string:"")
+//    let cachePolicy = NSURLRequestCachePolicy.ReloadIgnoringLocalCacheData
+//
+//    var request = NSMutableURLRequest(URL: url!, cachePolicy: cachePolicy, timeoutInterval: 2.0)
+//    request.HTTPMethod = "POST"
+    
+    struct PlantData {
+        
+    }
+    
+    func request() -> String {
+        return ""
+    }
+    
+    func parseRequest(json: String) -> [PlantData]{
+        return PlantData();
+    }
+}
---
+++ My plant and me/plantModel.swift
@@ -0,0 +1,21 @@
+//
+//  plantModel.swift
+//  My plant and me
+//
+//  Created by zephyz on 18/04/15.
+//  Copyright (c) 2015 purplicious. All rights reserved.
+//
+
+import Foundation
+
+class PlantManager {
+    
+    func waterPlant(quantity: Int) {
+        
+    }
+    
+    func setLights(minutes duration: Int) {
+        
+        
+    }
+}
